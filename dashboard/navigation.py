fdeeb69f793ad200d5cb3bd9edc14f7ca9aa61fc
Nikhil Rao <nikhil@reflex.dev>
2024-04-04 15:26:52 -0700

    Add icons

--- dashboard/navigation.py
+++ dashboard/navigation.py
@@ -6,20 +6,19 @@ from dashboard.styles import FONT_FAMILY
 
 def sidebar_link(text: str, href: str, icon: str):
     return rx.link(
-        text,
-        # rx.flex(
-        #     rx.icon_button(
-        #         lucide.icon(tag=icon, weight=16, height=16),
-        #         variant="soft",
-        #     ),
-        #     text,
-        #     py="2",
-        #     px="4",
-        #     gap="4",
-        #     align="baseline",
-        #     direction="row",
-        #     font_family=FONT_FAMILY,
-        # ),
+        rx.flex(
+            rx.icon_button(
+                rx.icon(tag=icon, weight=16, height=16),
+                variant="soft",
+            ),
+            text,
+            py="2",
+            px="4",
+            spacing="3",
+            align="baseline",
+            direction="row",
+            font_family=FONT_FAMILY,
+        ),
         href=href,
         width="100%",
         border_radius="8px",
